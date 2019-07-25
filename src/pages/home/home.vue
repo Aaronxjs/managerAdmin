7a2b2e9c1687708ba96d7b5e860ba830117510ff
Aaronxjs <1556300171@qq.com>
2019-07-25 22:58:29 +0800

    xjs

---
+++ src/pages/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>首页</p>
+  </div>
+</template>
+<script>
+  export default {
+    mounted () {
+//      this.$store.commit('setBreadlist', [{path: '/home', meta: {title: '首页'}}]);
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .home{
+
+  }
+</style>
